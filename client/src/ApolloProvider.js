023829a35a476139252833fc30df3ee4268c23fb
Ashwani Yadav <ashyadavash@gmail.com>
2020-11-03 23:11:21 +0530

    Update ApolloProvider.js

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://ec2-18-221-188-29.us-east-2.compute.amazonaws.com:5000'
+  uri: 'http://localhost:5000'
 });
 
 const authLink = setContext(() => {
